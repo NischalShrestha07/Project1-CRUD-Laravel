1eb2348e861fd46430e440f519f78fff4341380c
Nischal <nischalshrestha800@gmail.com>
2024-07-01 12:06:07 +0545

    Employee CRUD

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -8,6 +8,6 @@ class EmployeeController extends Controller
 {
     public function index()
     {
-        echo "This is just for testing";
+        return view("employee.list");
     }
 }
---
+++ resources/views/employee/list.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SIMPLE LARAVEL 9 CRUD </title>
+    <link rel="stylesheet" href="{{assets}}">//
+    {{-- download the assets
+    --}}
+</head>
+
+<body>
+
+    <div class="bg-dark py-3">
+        <div class="container">
+            <div class="h4 text-white">SSIMPLE LARAVEL 11 CRUD</div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="d-flex">
+            <div>Employee</div>
+        </div>
+
+    </div>
+</body>
+
+</html>
